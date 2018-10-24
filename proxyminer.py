58fffebd66eab8269ae211f84f32dbc9f6cf5c06
Vay3t <fffyanez@gmail.com>
2018-10-24 12:28:47 -0300

    Add files via upload

--- proxyminer.py
+++ proxyminer.py
@@ -5,16 +5,16 @@ from bs4 import BeautifulSoup
 r = requests.get("https://www.socks-proxy.net")
 soup = BeautifulSoup(r.text, 'html.parser')
 
-lista = []
-for info in soup.find_all('td'):
-	ip_candidates = re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\d{2,5}\b", str(info))
-	scanSocks = re.findall(r"Socks[4-5]",str(info))
-	if ip_candidates:
-		lista.append(ip_candidates[0])
-	elif scanSocks:
-		lista.append(scanSocks[0])
+data = []
+table = soup.find('table', attrs={'id':'proxylisttable'})
 
-a = 0
-while a < len(lista):
-	print(lista[a+2] + " " + lista[a] + " " + lista[a+1])
-	a += 3
+rows = table.find_all('tr')
+for row in rows:
+    cols = row.find_all('td')
+    cols = [ele.text.strip() for ele in cols]
+    data.append([ele for ele in cols if ele]) # Get rid of empty values
+
+for elemento in data:
+	if elemento:
+		print(elemento[4]+ " " +elemento[0]+ " " +elemento[1]+ " # " +elemento[2])
+
