b1b1e1f0584688654850f62585f6526f496f8414
vay3t <vay3t@github.com>
2019-02-15 18:06:14 -0300

    add check proxy option

--- proxyminer.py
+++ proxyminer.py
@@ -1,20 +1,81 @@
 import requests
 from bs4 import BeautifulSoup
+import sys
 
-r = requests.get("https://www.socks-proxy.net")
-soup = BeautifulSoup(r.text, 'html.parser')
 
-data = []
-table = soup.find('table', attrs={'id':'proxylisttable'})
+def obtainProxies():
+	r = requests.get("https://www.socks-proxy.net")
+	soup = BeautifulSoup(r.text, 'html.parser')
 
-rows = table.find_all('tr')
-for row in rows:
-    cols = row.find_all('td')
-    cols = [ele.text.strip() for ele in cols]
-    data.append([ele for ele in cols if ele]) # Get rid of empty values
+	data = []
+	table = soup.find('table', attrs={'id':'proxylisttable'})
 
-for elemento in data:
-	if elemento:
-		#print(elemento)
-		print(elemento[4].lower()+ " " +elemento[0]+ " " +elemento[1]+ " # " +elemento[2])
+	rows = table.find_all('tr')
+	for row in rows:
+		cols = row.find_all('td')
+		cols = [ele.text.strip() for ele in cols]
+		data.append([ele for ele in cols if ele]) # Get rid of empty values
 
+
+	allProxys = []
+
+	for elemento in data:
+		if elemento:
+			listOneProxy = []
+
+			typeSocks = elemento[4].lower()
+			if typeSocks != ("socks4" or "socks5"):
+				continue
+			ipProxy = elemento[0]
+			portProxy = elemento[1]
+			countryCode = elemento[2]
+
+			listOneProxy.append(typeSocks)
+			listOneProxy.append(ipProxy)
+			listOneProxy.append(portProxy)
+			listOneProxy.append(countryCode)
+
+			#print("{0} {1} {2} # {3}".format(typeSocks,ipProxy,portProxy,countryCode))
+			allProxys.append(listOneProxy)
+
+	return allProxys
+
+
+def checkProxy(lista):
+	buildProxy = "{0}://{1}:{2}".format(lista[0],lista[1],lista[2])
+	printProxy = "{0} {1} {2} # {3}".format(lista[0],lista[1],lista[2],lista[3])
+	proxy = {'http': buildProxy}
+	try:
+		requests.get('http://www.google.com', proxies=proxy)
+		print(printProxy+" ---> ON")
+	except requests.exceptions.ConnectionError:
+		print(printProxy+" ---> OFF")
+
+
+def help():
+	print("""usage: python3 """ + sys.argv[0] + """ <option>
+		
+	help - Show help
+	lister - Obtain list of proxies
+	ckecker - Obtain list of proxies and check if live
+""")
+
+
+try:
+	if len(sys.argv) != 2:
+		help()
+	else:
+		if sys.argv[1] == "help":
+			help()
+		elif sys.argv[1] == "lister":
+			for proxy in obtainProxies():
+				print("{0} {1} {2} # {3}".format(proxy[0],proxy[1],proxy[2],proxy[3]))
+		elif sys.argv[1] == "checker":
+			for proxyList in obtainProxies():
+				checkProxy(proxyList)
+
+except KeyboardInterrupt:
+	sys.exit(0)
+
+except Exception as e:
+	print(e)
